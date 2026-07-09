f891bb7f6192f151ae1619dce782bcb2842ae6a6
MarkzJR10 <markozleal09@gmail.com>
2026-07-09 00:04:01 -0600

    fix: adjust script name in public/index.php for subfolders

--- public/index.php
+++ public/index.php
@@ -1,5 +1,13 @@
 <?php
 
+// Ajustar SCRIPT_NAME y PHP_SELF para soportar subcarpetas en cPanel sin /public en la URL
+if (isset($_SERVER['SCRIPT_NAME']) && strpos($_SERVER['SCRIPT_NAME'], '/public/index.php') !== false) {
+    $_SERVER['SCRIPT_NAME'] = str_replace('/public/index.php', '/index.php', $_SERVER['SCRIPT_NAME']);
+}
+if (isset($_SERVER['PHP_SELF']) && strpos($_SERVER['PHP_SELF'], '/public/index.php') !== false) {
+    $_SERVER['PHP_SELF'] = str_replace('/public/index.php', '/index.php', $_SERVER['PHP_SELF']);
+}
+
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
