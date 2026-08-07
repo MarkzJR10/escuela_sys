fc279244ecbf4d918e963b6a3e5cf610bad19335
MarkzJR10 <markozleal09@gmail.com>
2026-08-07 10:00:29 -0600

    feat(contabilidad): agregar paginado DataTables en auditoria de ventas

--- resources/views/contabilidad/lista_ventas.blade.php
+++ resources/views/contabilidad/lista_ventas.blade.php
@@ -28,7 +28,7 @@
             
             <div class="card-body">
                 @if($pagos->count() > 0)
-                    <table class="table table-bordered table-striped table-hover data-table text-sm">
+                    <table id="ventas-table" class="table table-bordered table-striped table-hover text-sm">
                         <thead class="thead-dark">
                             <tr>
                                 <th>Folio Ticket</th>
@@ -77,4 +77,24 @@
         </div>
     </div>
 </div>
+@stop
+
+@section('plugins.Datatables', true)
+
+@section('js')
+<script>
+    $(document).ready(function() {
+        $('#ventas-table').DataTable({
+            "language": {
+                "url": "//cdn.datatables.net/plug-ins/1.10.20/i18n/Spanish.json"
+            },
+            "pageLength": 10,
+            "responsive": true,
+            "order": [[0, "desc"]],
+            "columnDefs": [
+                { "orderable": false, "targets": 6 }
+            ]
+        });
+    });
+</script>
 @stop
