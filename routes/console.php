52e3d71334e26a9fe57011876216c6aa76d0f992
MarkzJR10 <markozleal09@gmail.com>
2026-09-07 16:05:57 -0600

    fix: explicit America/Mexico_City timezone for recargos schedule

--- routes/console.php
+++ routes/console.php
@@ -8,5 +8,5 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('generate:adeudos')->monthlyOn(1, '00:00');
-Schedule::command('generate:adeudos')->monthlyOn(11, '00:00');
+Schedule::command('generate:adeudos')->monthlyOn(1, '00:00')->timezone('America/Mexico_City');
+Schedule::command('generate:adeudos')->monthlyOn(11, '00:00')->timezone('America/Mexico_City');
